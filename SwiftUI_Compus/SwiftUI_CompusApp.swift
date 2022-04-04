e54ce021b1570dd0b3160779cc857b95c3cc3560
trsimanto <simanto14feb@gmail.com>
2022-04-04 03:09:20 -0400

    init

---
+++ SwiftUI_Compus/SwiftUI_CompusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CompusApp.swift
+//  SwiftUI_Compus
+//
+//  Created by Towhid on 4/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CompusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
